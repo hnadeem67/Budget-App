1849456b47d45542870e5fbaf634c6d4031e1437
emanueltalmi <etalmi11@gmail.com>
2020-11-17 21:42:38 -0700

    Added more to the profile UI

--- BudgetApp-master/App.js
+++ BudgetApp-master/App.js
@@ -1,11 +1,16 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as React from "react";
+import { Image, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
+import "./profileScreen.css";
+import logo from "./app/assets/man.png";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home!</Text>
     </View>
   );
@@ -13,7 +18,7 @@ function HomeScreen() {
 
 function ReceiptScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Receipt!</Text>
     </View>
   );
@@ -21,12 +26,46 @@ function ReceiptScreen() {
 
 function ProfileScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{ padding: 20, alignItems: "center", justifyContent: "center" }}
+      >
+        <Image source={logo} style={{ height: 100, width: 100 }}></Image>
+      </View>
+
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ paddingBottom: "5%", fontSize: 25 }}>Profile Name</Text>
+      </View>
+
+      <View style={{ paddingLeft: "5%" }}>
+        <Text style={{ color: "#e91e63" }}>Account Settings</Text>
+      </View>
+
+      <div className="btn-group">
+        <ul style={{ listStyle: "none" }}>
+          <li>
+            <button>Change Name</button>
+          </li>
+          <li>
+            <button>Change Address</button>
+          </li>
+          <li>
+            <button>Change Phone Number</button>
+          </li>
+          <li>
+            <button>Change Email</button>
+          </li>
+        </ul>
+      </div>
+
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <div className="deleteButton">
+          <button>Delete Account</button>
+        </div>
+      </View>
     </View>
   );
 }
-
 
 const Tab = createBottomTabNavigator();
 
@@ -35,14 +74,14 @@ function MyTabs() {
     <Tab.Navigator
       initialRouteName="HomeScreen"
       tabBarOptions={{
-        activeTintColor: '#e91e63',
+        activeTintColor: "#e91e63",
       }}
     >
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
@@ -52,7 +91,7 @@ function MyTabs() {
         name="Receipt"
         component={ReceiptScreen}
         options={{
-          tabBarLabel: 'Add',
+          tabBarLabel: "Add",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="plus" color={color} size={size} />
           ),
@@ -62,7 +101,7 @@ function MyTabs() {
         name="Profile"
         component={ProfileScreen}
         options={{
-          tabBarLabel: 'Profile',
+          tabBarLabel: "Profile",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
           ),
--- BudgetApp-master/app/screens/components/AppButton.js
+++ BudgetApp-master/app/screens/components/AppButton.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import {StyleSheet,Text, TouchableOpacity} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-function AppButton({ title, onPress, }) {
-    return (
-        <TouchableOpacity style={[styles.button, {backgroundColor: "#45C5D9"}]} onPress={onPress}>      
-           <Text style={styles.text}>{title}</Text>
-       </TouchableOpacity>
-    );
+function AppButton({ title, onPress }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: "#45C5D9" }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#11B5E4',
-        borderRadius:25,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 15,
-        width: '100%',
-        marginVertical: 5
-    },
-    text:{
-        color: '#fff',
-        fontSize: 18,
-        textTransform: 'uppercase',
-        fontWeight:'bold',
-        
-    }
-})
+  button: {
+    backgroundColor: "#11B5E4",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+    marginVertical: 5,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
 
 export default AppButton;
